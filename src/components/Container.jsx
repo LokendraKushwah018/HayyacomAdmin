3896aa2236b48d2c146a9d6f043d69cf4aa1b32c
Lokendra <kushwahalokendra018@gmail.com>
2023-02-09 13:57:05 +0530

    first commit

---
+++ src/components/Container.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Footer from './Footer'
+import Header from './Header'
+import Sidebar from './Sidebar'
+
+const Container = ({children}) => {
+  return (
+        <>
+        <div className='wrapper'>
+        <Header />
+            <Sidebar />    
+          
+            {children}        
+       
+        <Footer />
+     
+        </div>
+      </>
+  )
+}
+
+export default Container
+
